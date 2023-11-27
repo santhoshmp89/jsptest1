75133e4325810dcda53f36df1d549feaf0613fe8
Santhosh Kumar <santhoshmp89@gmail.com>
2023-11-27 10:39:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ var ajax = [
   'https://dummyjson.com/products/1',
   'https://dummyjson.com/products/2',
   'https://dummyjson.com/products/3',
-  'https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap',
-  'https://developers.google.com/_pwa/developers/icons/icon-144x144.png',
-  'https://www.google.com/xjs/_/js/md=3/k=xjs.hd.en.iXdF5vQX3t8.O/ck=xjs.hd.3mhtCf28lXo.L.W.O/am=BAAAAAAAAAAAAAAAAAAAAAQAAAAAEDUQDgFsgACBABBggAAgAICAFKQQBAMAEPBQJgAAACZAYAgYAogKPOcBAJBAFQAAAAAAEAwiAAAACAAAoAMAACCgEcAAhIBKQAAAAADyACA4ABhEEAAAAAAAAAAAAAABTBAMLkgAFAQQAAAAAAAAAAAAAKSkycVAAgAK/rs=ACT90oF-uCPSsFsxixtFlrmZcVrsn9STJQ'
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap'
 ];
 
 ajax.forEach((a) => {
