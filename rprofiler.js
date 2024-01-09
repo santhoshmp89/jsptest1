e6499bf6b0293e9398a5061faf9d4089e50b414a
Santhosh Kumar <santhoshmp89@gmail.com>
2024-01-09 15:50:49 +0530

    Update rprofiler.js

--- rprofiler.js
+++ rprofiler.js
@@ -725,7 +725,7 @@ var AjaxRequestsHandler = (function () {
 var RProfiler = (function () {
     function RProfiler() {
         var _this = this;
-        this.restUrl = "g.3gl.net/jp/8710/v3.3.10/M";
+        this.restUrl = "portalstage.catchpoint.com/jp/1826/v3.3.10/M";
         this.startTime = (new Date()).getTime();
         this.eventsTimingHandler = new EventsTimingHandler();
         this.inputDelay = new InputDelayHandler();
