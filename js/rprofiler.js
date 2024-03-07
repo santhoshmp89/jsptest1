f3ee62ebc9fbdc510b34a876b14a924ae4475783
Santhosh Kumar <santhoshmp89@gmail.com>
2024-03-07 20:58:09 +0530

    Update rprofiler.js

--- js/rprofiler.js
+++ js/rprofiler.js
@@ -674,6 +674,17 @@ var ProfilerJsError = /** @class */ (function () {
 /* harmony default export */ const rprofiler_ProfilerJsError = (ProfilerJsError);
 
 ;// CONCATENATED MODULE: ./src/main/config.ts
+var __assign = (undefined && undefined.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var MainConfig = /** @class */ (function () {
     function MainConfig() {
     }
@@ -699,6 +710,9 @@ var MainConfig = /** @class */ (function () {
             Error: (_e = _f.windowEvent) === null || _e === void 0 ? void 0 : _e.Error
         };
     };
+    MainConfig.setAppConfig = function (config) {
+        _f.config = __assign(__assign({}, _f.config), config);
+    };
     MainConfig.initValues = function () {
         _f.setWindowEvent();
         _f.setRProfiler();
@@ -722,17 +736,17 @@ var MainConfig = /** @class */ (function () {
     MainConfig.hasPerformanceApi = !!_f.pageWindow.performance && typeof _f.pageWindow.performance === 'object';
     MainConfig.hasGetEntriesApi = _f.hasPerformanceApi && typeof _f.pageWindow.performance.getEntriesByType === 'function';
     MainConfig.testUserId = 123;
-    // version: string = '{{version}}';
-    MainConfig.version = 'v3.3.11';
+    MainConfig.version = '{{version}}';
+    // static version: string = 'v3.3.11';
     MainConfig.config = {
-        // sampleRate: -999, // range [0 - 100]
-        sampleRate: 100, // range [0 - 100]
+        sampleRate: -999, // range [0 - 100]
+        // sampleRate: 100, // range [0 - 100]
         // waterfallSampleRate: -888, // range [0 - 100]
         waterfallSampleRate: 10, // range [0 - 100]
         // postUrl: protocol + '{{postUrl}}',
         postUrl: _f.protocol + 'lst01a.3genlabs.net/hawklogserver/r.p',
-        // siteId: -111,
-        siteId: 1826,
+        siteId: -111,
+        // siteId: 1826,
         // debugParameter: '{{debugParam}}',
         debugParameter: 'GlimpseDebug',
         // debugUrl: '{{debugUrl}}',
@@ -2487,6 +2501,42 @@ var DataProvider = /** @class */ (function () {
 /* harmony default export */ const main_DataProvider = (DataProvider);
 
 ;// CONCATENATED MODULE: ./src/main/main.ts
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 
 
 var mainScript = function () {
@@ -2502,6 +2552,24 @@ var mainScript = function () {
     if (!!config.pageWindow['__cpSendOnLoad']) {
         config.config.sendOnLoad = config.pageWindow['__cpSendOnLoad'] === true;
     }
+    var getAppDetails = function () { return __awaiter(void 0, void 0, void 0, function () {
+        var response, data;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, fetch('https://jsonplaceholder.typicode.com/todos/1')];
+                case 1:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    data = _a.sent();
+                    return [2 /*return*/, data];
+            }
+        });
+    }); };
+    // @ts-ignore
+    var appDetails = getAppDetails();
+    config.setVersion('v3.3.11');
+    config.setAppConfig({ siteId: 1826, sampleRate: 100 });
     // @ts-ignore
     var provider = new main_DataProvider();
 };
